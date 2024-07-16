05c07f19073ff9fc9483e863d985b0f2351fd8a0
Almosk <alvin.kita@outlook.fr>
2024-07-16 10:56:59 +0200

    Affichage des questions plus propre

--- app/src/main/java/com/tp/filmquiz/MainActivity.java
+++ app/src/main/java/com/tp/filmquiz/MainActivity.java
@@ -1,25 +1,140 @@
 package com.tp.filmquiz;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.tp.filmquiz.pojo.Question;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    // Declaration du context
+    private Context context;
+
+
+    // Declaration de la liste de questions
+    final List<Question> questions = new ArrayList<>();
+
+    //Déclaration du compteur de question et de score
+    private int questionCounter = 0;
+    private int score = 0;
+
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button buttonFalse;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //setContentView(R.layout.constrait_layout);
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
+
+        // Initialisation du context
+        context = getApplicationContext();
+
+        // Recuperation des questions
+        questions.add(new Question(getString(R.string.question_ai), true));
+        questions.add(new Question(getString(R.string.question_taxi_driver), true));
+        questions.add(new Question(getString(R.string.question_2001), false));
+        questions.add(new Question(getString(R.string.question_reservoir_dogs), true));
+        questions.add(new Question(getString(R.string.question_citizen_kane), false));
+
+        //Initialisation des objets
+
+        // Boutons
+        // Declaration des buttons
+        Button buttonTrue = findViewById(R.id.buttonTrue);
+        buttonFalse = findViewById(R.id.buttonFalse);
+
+        // Question
+        TextView tvQuestion = findViewById(R.id.question);
+        tvQuestion.setText(questions.get(questionCounter).getQuestion());
+
+        // Score
+        TextView tvScore = findViewById(R.id.score);
+        tvScore.setText("Score : " + score);
+
+        // Gestion du click sur buttonTrue
+        buttonTrue.setOnClickListener(v -> {
+            if (questions.get(questionCounter).isAnswer()) {
+                context = getApplicationContext();
+                CharSequence text = "Bonne réponse";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                score++;
+                incrementScore(score, tvScore);
+            } else {
+                context = getApplicationContext();
+                CharSequence text = "Mauvaise réponse";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+            questionCounter++;
+            nextQuestion(questionCounter, questions, tvQuestion, buttonTrue, buttonFalse);
+        });
+
+        // Gestion du click sur buttonFalse
+        buttonFalse.setOnClickListener(v -> {
+            if (questions.get(questionCounter).isAnswer()) {
+                context = getApplicationContext();
+                CharSequence text = "Mauvaise réponse";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            } else {
+                context = getApplicationContext();
+                CharSequence text = "Bonne réponse";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                score++;
+                incrementScore(score, tvScore);
+            }
+            questionCounter++;
+            nextQuestion(questionCounter, questions, tvQuestion, buttonTrue, buttonFalse);
+        });
     }
+
+    /**
+     * Méthode pour passer à la question suivante
+     * @param questionCounter : le compteur de question
+     * @param questions : la liste de questions
+     * @param tvQuestion : le TextView de la question
+     * @param buttonTrue : le bouton vrai
+     * @param buttonFalse : le bouton faux
+     */
+    @SuppressLint("SetTextI18n")
+    private void nextQuestion(int questionCounter, List<Question> questions, TextView tvQuestion, Button buttonTrue, Button buttonFalse) {
+        if (questionCounter < questions.size()) {
+            tvQuestion.setText(questions.get(questionCounter).getQuestion());
+        } else {
+            tvQuestion.setText("Fin du quiz");
+            buttonTrue.setVisibility(View.INVISIBLE);
+            buttonFalse.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /**
+     * Méthode pour incrémenter le score
+     * @param score : le score
+     * @param tvScore : le TextView du score
+     */
+    private void incrementScore(int score, TextView tvScore) {
+        tvScore.setText(MessageFormat.format("Score : {0}", score));
+    }
+
 }
---
+++ app/src/main/java/com/tp/filmquiz/pojo/Question.java
@@ -0,0 +1,34 @@
+package com.tp.filmquiz.pojo;
+
+public class Question {
+
+    private int id;
+    private String question;
+    private boolean answer;
+
+    public Question(String question, boolean answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+}
